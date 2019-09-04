d0fe3cd8b3bfd1a2255ff2d10e02c89cea29b7ea
David Hirtz <david.hirtz@me.com>
2019-09-04 18:14:31 +0200

    Update Bootstrap.php

--- composer/Bootstrap.php
+++ composer/Bootstrap.php
@@ -6,7 +6,6 @@ use davidhirtz\yii2\skeleton\composer\BootstrapTrait;
 use davidhirtz\yii2\skeleton\web\Application;
 use yii\base\BootstrapInterface;
 use Yii;
-use davidhirtz\yii2\skeleton\console\controllers\MigrateController;
 
 /**
  * Class Bootstrap
