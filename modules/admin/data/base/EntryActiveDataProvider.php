5dbba77b41a9ecf82433607a20369be1033c1b0e
David Hirtz <david.hirtz@me.com>
2020-02-19 11:13:16 +0100

    Update EntryActiveDataProvider.php

--- modules/admin/data/base/EntryActiveDataProvider.php
+++ modules/admin/data/base/EntryActiveDataProvider.php
@@ -7,7 +7,6 @@ use davidhirtz\yii2\cms\models\queries\EntryQuery;
 use davidhirtz\yii2\cms\models\Category;
 use davidhirtz\yii2\cms\models\Entry;
 use davidhirtz\yii2\cms\modules\ModuleTrait;
-use davidhirtz\yii2\skeleton\db\ActiveQuery;
 use yii\data\ActiveDataProvider;
 
 /**
