5e758aa7c04b35b9b2e8b1366fe2edea431b256a
David Hirtz <david.hirtz@me.com>
2020-08-27 10:24:29 +0200

    Update ActiveForm.php

--- modules/admin/widgets/forms/ActiveForm.php
+++ modules/admin/widgets/forms/ActiveForm.php
@@ -7,8 +7,8 @@ use davidhirtz\yii2\cms\models\Category;
 use davidhirtz\yii2\cms\models\Entry;
 use davidhirtz\yii2\cms\models\Section;
 use davidhirtz\yii2\skeleton\helpers\Html;
+use davidhirtz\yii2\skeleton\modules\admin\widgets\forms\ModelTimestampTrait;
 use davidhirtz\yii2\skeleton\widgets\forms\CKEditor;
-use davidhirtz\yii2\timeago\Timeago;
 use Yii;
 use yii\helpers\ArrayHelper;
 
@@ -20,6 +20,8 @@ use yii\helpers\ArrayHelper;
  */
 class ActiveForm extends \davidhirtz\yii2\skeleton\widgets\bootstrap\ActiveForm
 {
+    use ModelTimestampTrait;
+
     /**
      * @var bool
      */
@@ -109,26 +111,7 @@ class ActiveForm extends \davidhirtz\yii2\skeleton\widgets\bootstrap\ActiveForm
      */
     protected function getFooterItems(): array
     {
-        $items = [];
-
-        if (!$this->model->getIsNewRecord()) {
-            if ($this->model->updated_by_user_id) {
-                $items[] = Yii::t('skeleton', 'Last updated by {user} {timestamp}', [
-                    'timestamp' => Timeago::tag($this->model->updated_at),
-                    'user' => Html::username($this->model->updated, Yii::$app->getUser()->can('userUpdate', ['user' => $this->model->updated]) ? ['/admin/user/update', 'id' => $this->model->updated_by_user_id] : null),
-                ]);
-            } else {
-                $items[] = Yii::t('skeleton', 'Last updated {timestamp}', [
-                    'timestamp' => Timeago::tag($this->model->updated_at),
-                ]);
-            }
-
-            $items[] = Yii::t('skeleton', 'Created {timestamp}', [
-                'timestamp' => Timeago::tag($this->model->created_at),
-            ]);
-        }
-
-        return $items;
+        return $this->getTimestampItems();
     }
 
     /**
