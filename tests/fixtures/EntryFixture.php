1a2dcfc7cfee66bcc658617fa8a929252dbaf161
davidhirtz <david.hirtz@me.com>
2023-12-18 22:29:15 +0100

    Tests

---
+++ tests/fixtures/EntryFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace davidhirtz\yii2\cms\tests\fixtures;
+
+use davidhirtz\yii2\cms\models\Entry;
+use yii\test\ActiveFixture;
+
+class EntryFixture extends ActiveFixture
+{
+    public $modelClass = Entry::class;
+}
