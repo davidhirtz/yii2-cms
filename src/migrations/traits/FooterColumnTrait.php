1b5d3cb4190f6dcc672211f00177892f8b7f6001
davidhirtz <david.hirtz@me.com>
2024-01-04 10:36:11 +0100

    Menu  index

--- src/migrations/traits/FooterColumnTrait.php
+++ src/migrations/traits/FooterColumnTrait.php
@@ -21,6 +21,19 @@ trait FooterColumnTrait
                 ->defaultValue(false)
                 ->after('publish_date'));
         });
+
+        $schema = $this->getDb()->getSchema();
+
+        if ($schema->getTableSchema(Entry::tableName())->getColumn('show_in_menu')) {
+            $this->dropIndex('show_in_menu', Entry::tableName());
+
+            $this->createIndex('show_in_menu', Entry::tableName(), [
+                'show_in_menu',
+                'show_in_footer',
+                'status',
+                'position',
+            ]);
+        }
     }
 
     protected function dropShowInFooterColumn(): void
--- src/migrations/traits/MenuColumnTrait.php
+++ src/migrations/traits/MenuColumnTrait.php
@@ -21,11 +21,14 @@ trait MenuColumnTrait
                 ->defaultValue(false)
                 ->after('publish_date'));
         });
+
+        $this->createIndex('show_in_menu', Entry::tableName(), ['show_in_menu', 'status', 'position']);
     }
 
     protected function dropShowInMenuColumn(): void
     {
         $this->i18nTablesCallback(function () {
+            $this->dropIndex('show_in_menu', Entry::tableName());
             $this->dropColumn(Entry::tableName(), 'show_in_menu');
         });
     }
